4d233ab2f95554326f70fdbe6c3136f2e8519f04
FerminDRA <zs19004879@estudiantes.uv.mx>
2023-06-11 18:51:27 -0600

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const v1StudentRouter=require("./v1/routes/estudiantesRoutes");
 require('dotenv').config();
+const cors = require('cors');
 
 const app= express();
 const PORT= process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1/estudiantes",v1StudentRouter);
